0e66b4cdb8ff7006795809e796a16ddf24e3e662
James Davies <James_D_1994@live.co.uk>
2020-12-20 18:09:54 +0000

    Add result data getter/setter to model

--- src/abstract/model/Model.ts
+++ src/abstract/model/Model.ts
@@ -1,13 +1,24 @@
+import {ResultData} from "../data/ResultData";
+
 export class Model{
 
+    private resultData: ResultData;
     private spinsRemaining: number;
 
     constructor(){
 
     }
 
+    public setResultData(data: ResultData): void {
+        this.resultData = data;
+    }
+
     public setSpinsRemaining(spins: number): void {
         this.spinsRemaining = spins;
+    }
+
+    public getResultData(): ResultData {
+        return this.resultData;
     }
 
     public getSpinsRemaining(): number {
