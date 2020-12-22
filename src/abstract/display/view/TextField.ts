9bfdca7737c7b29fe3185403adb4356f36554059
James Davies <James_D_1994@live.co.uk>
2020-12-22 20:54:23 +0000

    Implemented dynamic text changes

--- src/abstract/display/view/TextField.ts
+++ src/abstract/display/view/TextField.ts
@@ -3,18 +3,25 @@ import {AbstractViewProperties} from "../properties/AbstractViewProperties";
 import {TextFieldProperties} from "../properties/TextFieldProperties";
 import {Font} from "../../font/Font";
 import {AlignConstants} from "../../constants/AlignConstants";
+import {KeyValuePair} from "./KeyValuePair";
 
 export class TextField extends AbstractGameView {
 
     protected properties: TextFieldProperties;
 
+    protected lang: string;
     protected boundingBox: PIXI.Graphics;
-    protected text: PIXI.Text;
+    protected field: PIXI.Text;
     protected style: PIXI.TextStyle;
 
     protected createProperties(properties: AbstractViewProperties) {
         super.createProperties(properties);
-        this.properties = <TextFieldProperties> properties;
+        this.properties = <TextFieldProperties>properties;
+    }
+
+    protected initialise() {
+        super.initialise();
+        this.lang = this.properties.lang;
     }
 
     protected create(): void {
@@ -37,51 +44,49 @@ export class TextField extends AbstractGameView {
     protected createStyle(): void {
         let font: Font = this.model.getFont(this.properties.fontStyle);
         this.style = font.getStyle();
-        if(this.properties.multiline){
+        if (this.properties.multiline) {
             this.setMultiline(this.properties.size.x, this.properties.horizontalAlign, 1, this.properties.leading);
         }
     }
 
     protected createText(): void {
-        this.text = new PIXI.Text(this.properties.lang, this.style);
-        this.addChild(this.text);
+        this.field = new PIXI.Text(this.properties.lang, this.style);
+        this.addChild(this.field);
     }
 
     protected updateAlign(): void {
-        let heightDiff = this.boundingBox.height - this.text.height;
-        let widthDiff = this.boundingBox.width - this.text.width;
-        let offset: PIXI.Point = new PIXI.Point(0,0);
-        switch(this.properties.horizontalAlign){
+        let heightDiff = this.boundingBox.height - this.field.height;
+        let widthDiff = this.boundingBox.width - this.field.width;
+        let offset: PIXI.Point = new PIXI.Point(0, 0);
+        switch (this.properties.horizontalAlign) {
             case AlignConstants.LEFT:
                 offset.x = 0;
                 break;
             case AlignConstants.CENTER:
-                offset.x = widthDiff/2
+                offset.x = widthDiff / 2
                 break;
             case AlignConstants.RIGHT:
                 offset.x = widthDiff;
                 break;
             default:
                 throw new Error("Did not recognise horizontal align type of '" + this.properties.horizontalAlign + "'");
-                break;
         }
 
-        switch(this.properties.verticalAlign){
+        switch (this.properties.verticalAlign) {
             case AlignConstants.TOP:
                 offset.y = 0;
                 break;
             case AlignConstants.CENTER:
-                offset.y = heightDiff/2
+                offset.y = heightDiff / 2
                 break;
             case AlignConstants.BOTTOM:
                 offset.y = heightDiff;
                 break;
             default:
                 throw new Error("Did not recognise vertical align type of '" + this.properties.verticalAlign + "'");
-                break;
         }
 
-        this.text.position = offset;
+        this.field.position = offset;
     }
 
     protected setMultiline(wrapWidth: number, align: string, lineHeight: number, leading: number): void {
@@ -93,8 +98,20 @@ export class TextField extends AbstractGameView {
         this.style.leading = leading;
     }
 
-    public setText(lang: string): void {
-        this.text.text = lang;
+    public setText(lang: string, update?: boolean): void {
+        this.lang = lang;
+        this.field.text = this.lang;
+        if(update){
+            this.updateAlign();
+        }
+    }
+
+    public setDynamicText(keyValuePair: KeyValuePair<string, string>, update?: boolean): void {
+        this.lang = this.lang.replace(keyValuePair.getKey(), keyValuePair.getValue());
+        this.field.text = this.lang;
+        if(update){
+            this.updateAlign();
+        }
     }
 
 }
