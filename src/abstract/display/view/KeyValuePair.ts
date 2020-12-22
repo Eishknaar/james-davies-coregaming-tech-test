29fd255cccafd94bcff7e563a7ba7ff06054b301
James Davies <James_D_1994@live.co.uk>
2020-12-22 19:34:22 +0000

    Implement KeyValuePair

---
+++ src/abstract/display/view/KeyValuePair.ts
@@ -0,0 +1,19 @@
+export class KeyValuePair <K, V> {
+
+    private key: K;
+    private value: V;
+
+    constructor(key: K, value: V){
+        this.key = key;
+        this.value = value;
+    }
+
+    public getKey(): K {
+        return this.key;
+    }
+
+    public getValue(): V {
+        return this.value;
+    }
+
+}
