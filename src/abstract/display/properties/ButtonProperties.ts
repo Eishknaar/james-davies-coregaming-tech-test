28dfc3b9d4592ee68e9485b9a33c34c7652219f4
James Davies <James_D_1994@live.co.uk>
2020-12-23 11:39:33 +0000

    Make Button text optional

--- src/abstract/display/properties/ButtonProperties.ts
+++ src/abstract/display/properties/ButtonProperties.ts
@@ -5,17 +5,21 @@ import {AlignConstants} from "../../constants/AlignConstants";
 export class ButtonProperties extends SpriteProperties {
 
     public textFieldProperties: TextFieldProperties;
+    public fontStyle: string;
+    public lang: string;
 
     public debug: boolean;
 
-    constructor(imageName: string, fontStyle: string, lang: string, position?: PIXI.Point) {
+    constructor(imageName: string, fontStyle?: string, lang?: string, position?: PIXI.Point) {
         super(imageName, position);
-        this.textFieldProperties = new TextFieldProperties(fontStyle, lang, new PIXI.Point(100,100), new PIXI.Point(0, 0), AlignConstants.CENTER, AlignConstants.CENTER);
 
     }
 
     protected setDefaultValues() {
         super.setDefaultValues();
+        if(this.fontStyle) {
+            this.textFieldProperties = new TextFieldProperties(this.fontStyle, this.lang, new PIXI.Point(100, 100), new PIXI.Point(0, 0), AlignConstants.CENTER, AlignConstants.CENTER);
+        }
         this.debug = false;
     }
 
