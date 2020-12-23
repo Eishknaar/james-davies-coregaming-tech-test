d6e8dcbdc81eefa56e42734fe0e62ef998069c61
James Davies <James_D_1994@live.co.uk>
2020-12-23 12:40:18 +0000

    Fix button text not appearing

--- src/abstract/display/properties/ButtonProperties.ts
+++ src/abstract/display/properties/ButtonProperties.ts
@@ -1,9 +1,12 @@
-import {SpriteProperties} from "./SpriteProperties";
 import {TextFieldProperties} from "./TextFieldProperties";
 import {AlignConstants} from "../../constants/AlignConstants";
+import {AbstractViewProperties} from "./AbstractViewProperties";
 
-export class ButtonProperties extends SpriteProperties {
+export class ButtonProperties extends AbstractViewProperties {
 
+
+    public imageName: string;
+    public fileType: string;
     public textFieldProperties: TextFieldProperties;
     public fontStyle: string;
     public lang: string;
@@ -11,16 +14,20 @@ export class ButtonProperties extends SpriteProperties {
     public debug: boolean;
 
     constructor(imageName: string, fontStyle?: string, lang?: string, position?: PIXI.Point) {
-        super(imageName, position);
+        super(position);
+        this.imageName = imageName;
+        this.fontStyle = fontStyle;
+        this.lang = lang;
+        this.setDefaultValues();
 
     }
 
     protected setDefaultValues() {
-        super.setDefaultValues();
+        this.fileType = ".png";
+        this.debug = false;
         if(this.fontStyle) {
             this.textFieldProperties = new TextFieldProperties(this.fontStyle, this.lang, new PIXI.Point(100, 100), new PIXI.Point(0, 0), AlignConstants.CENTER, AlignConstants.CENTER);
         }
-        this.debug = false;
     }
 
     public setDebug(value: boolean): void {
