1b078610dadd1eea33c86d4c4105bb84664b7a20
James Davies <James_D_1994@live.co.uk>
2020-12-23 11:41:09 +0000

    Add stake getter/setters in Model and populate with dummy stake values

--- src/abstract/model/Model.ts
+++ src/abstract/model/Model.ts
@@ -9,11 +9,14 @@ export class Model{
     private fontMap: FontMap;
     private resultData: ResultData;
     private spinsRemaining: number;
+    private stakeValues: number[];
+    private stake: number;
 
     constructor(){
         this.eventHandler = new EventEmitter();
         this.fontMap = new FontMap();
         this.setResultData(new ResultData({data: "data"}));
+        this.setStakeValues([0.20, 0.50, 1, 2, 5, 10]);
     }
 
     public setResultData(data: ResultData): void {
@@ -24,12 +27,28 @@ export class Model{
         this.spinsRemaining = spins;
     }
 
+    public setStakeValues(values: number[]): void {
+        this.stakeValues = values;
+    }
+
+    public setStake(value: number): void {
+        this.stake = value;
+    }
+
     public getResultData(): ResultData {
         return this.resultData;
     }
 
     public getSpinsRemaining(): number {
         return this.spinsRemaining;
+    }
+
+    public getStakeValues(): number[] {
+        return this.stakeValues;
+    }
+
+    public getStake(): number {
+        return this.stake;
     }
 
     public decrementSpins(): void {
