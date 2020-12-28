357a9733d565b56f40efa2c45e7d776e9d9cf4c2
James Davies <James_D_1994@live.co.uk>
2020-12-28 11:47:39 +0000

    Resize game to fit window

--- src/abstract/graphics/PixiManager.ts
+++ src/abstract/graphics/PixiManager.ts
@@ -4,12 +4,30 @@ class PixiManager {
     public canvas: HTMLCanvasElement;
     public stage: PIXI.Container;
 
+    private width: number;
+    private height: number;
+    private ratio: number;
+
     constructor(){
+        this.initialise();
+        this.setScene();
+        this.resize();
+        this.render();
+    }
+
+    private initialise(): void {
+        this.width = 1920;
+        this.height = 1080;
+        this.ratio = this.width/this.height;
+        window.onresize = () => this.resize();
+    }
+
+    private setScene(): void {
         this.renderer = PIXI.autoDetectRenderer({
-            width: 900,
-            height: 800,
-            transparent: true,
-            resolution: 1,
+            width: this.width,
+            height: this.height,
+            transparent: false,
+            resolution: this.ratio,
             antialias: true
         });
 
@@ -18,9 +36,27 @@ class PixiManager {
         document.body.appendChild(this.canvas);
 
         this.stage = new PIXI.Container();
-
-        this.render();
     }
+
+    private resize(): void {
+        let screenWidth: number = window.innerWidth;
+        let screenHeight: number = window.innerHeight;
+        let screenRatio: number = screenWidth/screenHeight;
+
+        let newWidth: number;
+        let newHeight: number;
+
+        if (screenRatio >= this.ratio) {
+            newWidth = window.innerHeight * this.ratio;
+            newHeight = window.innerHeight;
+        } else {
+            newWidth = window.innerWidth;
+            newHeight = window.innerWidth / this.ratio;
+        }
+        this.renderer.view.style.width = newWidth + 'px';
+        this.renderer.view.style.height = newHeight + 'px';
+    }
+
 
     private render(): void {
         this.renderer.render(this.stage);
